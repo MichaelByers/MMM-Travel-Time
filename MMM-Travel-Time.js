e10d3a07ef457d08f3c35ddcaf850b6026193e9d
MichaelByers <michael.byers@comcast.net>
2020-03-04 21:13:05 -0700

    Add files via upload

---
+++ MMM-Travel-Time.js
@@ -0,0 +1,107 @@
+/* Magic Mirror Module: MMM-Travel-Time
+ * Version: 1.0.0
+ *
+ * By Michael Byers https://github.com/MichaelByers/
+ * MIT Licensed.
+ */
+
+Module.register('MMM-Travel-Time', {
+
+	defaults: {
+            apikey:    '',
+			olat:		0,
+			olong: 		-1,
+			dlat: 		0,
+			dlong: 		-1,
+			glat:		0,
+			glong:		-1,
+            interval:   300000 // Every 5 mins
+        },
+
+    // Define required scripts.
+    getScripts: function() {
+        return ["moment.js"];
+    },
+
+    start:  function() {
+        Log.log('Starting module: ' + this.name);
+        var self = this;
+
+        // Set up the local values, here we construct the request url to use
+        this.loaded = false;
+        this.route = [];
+
+        // Trigger the first request
+        this.getRouteData(this);
+        setInterval(function() {
+            self.getRouteData(self);
+          }, self.config.interval);
+    },
+
+    getStyles: function() {
+        return ['MMM-Travel-Time.css', 'font-awesome.css'];
+    },
+
+
+    getRouteData: function(_this) {
+		var day = moment().format('dddd');
+ 
+		// if weekday show drive time to work
+		if ((day != 'Saturday') && (day != 'Sunday')) {
+			this.url = 'https://api.tomtom.com/routing/1/calculateRoute/' + this.config.olat + ','+ this.config.olong + ':' + this.config.dlat + ',' + this.config.dlong + '/json?computeTravelTimeFor=all&sectionType=traffic&traffic=true&travelMode=car&key=' + this.config.apikey;
+		} else {
+			this.url = 'https://api.tomtom.com/routing/1/calculateRoute/' + this.config.olat + ','+ this.config.olong + ':' + this.config.glat + ',' + this.config.glong + '/json?computeTravelTimeFor=all&sectionType=traffic&traffic=true&travelMode=car&key=' + this.config.apikey;
+		}
+			// Make the initial request to the helper then set up the timer to perform the updates
+        _this.sendSocketNotification('GET-TRAVEL-TIME', _this.url);
+    },
+
+
+    getDom: function() {
+        // Set up the local wrapper
+        var wrapper = null;
+
+
+        // If we have some data to display then build the results
+        if (this.loaded) {
+            wrapper = document.createElement('div');
+	 	    wrapper.className = 'route';
+			
+			var day = moment().format('dddd');
+			var title = 'Travel Time to';
+			var travelTime = '';
+			var text = '';
+
+			// if weekday show drive time to work
+			if ((day != 'Saturday') && (day != 'Sunday')) {
+				title = title + 'Work: ';
+			} else {
+				title = title + 'Golf: ';
+			}
+
+			travelTime = this.route.summary.travelTimeInSeconds;
+
+			text = title + travelTime + ' min';
+
+    		wrapper.innerHTML = text;
+			}
+        } else {
+            // Otherwise lets just use a simple div
+            wrapper = document.createElement('div');
+            wrapper.innerHTML = 'LOADING...';
+        }
+
+        return wrapper;
+    },
+
+
+    socketNotificationReceived: function(notification, payload) {
+        // check to see if the response was for us and used the same url
+        if (notification === 'GOT-TRAVEL-TIME' && payload.url === this.url) {
+                // we got some data so set the flag, stash the data to display then request the dom update
+                this.loaded = true;
+                this.route = payload.route;
+                this.updateDom(1000);
+            }
+        }
+    });
---
+++ node_helper.js
@@ -0,0 +1,45 @@
+/* Magic Mirror Module: MMM-Travel-Time helper
+ * Version: 1.0.0
+ *
+*/
+
+var NodeHelper = require('node_helper');
+var request = require('request');
+
+module.exports = NodeHelper.create({
+
+    start: function () {
+        console.log('MMM-Travel-Time helper, started...');
+    },
+
+
+    getRouteData: function(payload) {
+
+        var _this = this;
+        this.url = payload;
+
+        request({url: this.url, method: 'GET'}, function(error, response, body) {
+            // Lets convert the body into JSON
+            var result = JSON.parse(body);
+            var route = null; // Clear the array
+
+            // Check to see if we are error free and got an OK response
+            if (!error && response.statusCode == 200) { 
+                route = result.Routes[0]);
+            } else {
+                // In all other cases it's some other error
+            }
+
+            // We have the response figured out so lets fire off the notifiction
+            _this.sendSocketNotification('GOT-TRAVEL-TIME', {'url': _this.url, 'route': route});
+        });
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        // Check this is for us and if it is let's get the weather data
+        if (notification === 'GET-TRAVEL-TIME') {
+            this.getRouteData(payload);
+        }
+    }
+
+});
